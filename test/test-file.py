30bad24aed825d615ceaeb5660404a48dec8c10a
Andrei Puchko <penta@penta.by>
2021-07-29 01:27:00 +0200

    flake8 applied

--- test/test-file.py
+++ test/test-file.py
@@ -3,14 +3,23 @@ import os
 
 sys.path.insert(1, "../")
 
+
 from data2doc.data2doc import merge
 
-test_data_folder="../test-data/test01/"
-test_inpot_file_name1=f"{test_data_folder}test.docx"
-test_inpot_file_name2=f"{test_data_folder}test.xlsx"
-test_result_file_name="../test-result/test-result.docx"
+test_result_file_name = "test-result/test-result.docx"
+test_data_folder = "test-data/test01/"
 
-result=merge(test_inpot_file_name1,test_inpot_file_name2,test_result_file_name)
+if not os.path.exists("test-data"):
+    test_result_file_name = f"../{test_result_file_name}"
+    test_data_folder = f"../{test_data_folder}"
+
+test_input_file_name1 = f"{test_data_folder}test.docx"
+test_input_file_name2 = f"{test_data_folder}test.xlsx"
+
+
+result = merge(test_input_file_name1,
+               test_input_file_name2,
+               test_result_file_name)
 
 if result and "win" in sys.platform:
-    os.startfile(test_result_file_name.replace("/","\\"))
+    os.startfile(test_result_file_name.replace("/", "\\"))
--- test/test-folder.py
+++ test/test-folder.py
@@ -7,37 +7,41 @@ sys.path.insert(1, "../")
 
 from data2doc.data2doc import data2doc
 
-                             
-testResultFolder=f"../test-result/"
-testSourceFolder="../test-data/test*"
+testResultFolder = "test-result/"
+testSourceFolder = "test-data/test*"
 
-d2d=data2doc()
+if not os.path.exists("test-data"):
+    testResultFolder = f"../{testResultFolder}"
+    testSourceFolder = f"../{testSourceFolder}"
+
+
+d2d = data2doc()
 
 for testFolderName in glob.glob(testSourceFolder):
-	print (testFolderName," ==>> ", end = '')
-	ti0=time.time()
-	for x in glob.glob(f"{testFolderName}/*"):
-		d2d.loadFile(x)
-	rez=d2d.data2doc()
-	if rez:
-		print ("Ok")
-		tName=os.path.basename(testFolderName)
-		i=0
-		while True:
-			tFileName=f"{testResultFolder}{tName}-{i}.docx"
-			tLock=testResultFolder+".~lock."+tName+str(i)+".docx#"
-			print (tLock,glob.glob(tLock))
-			if glob.glob(tLock):
-				i+=1
-			else:
-				if glob.glob(tFileName):
-					os.remove(tFileName)
-				break
-		open(tFileName,"wb").write(d2d.docxResultBinary)
-	else:
-		print ("Fail")
-print ("found Excel format strings:")
+    print(testFolderName, " ==>> ", end='')
+    ti0 = time.time()
+    for x in glob.glob(f"{testFolderName}/*"):
+        d2d.loadFile(x)
+    rez = d2d.data2doc()
+    if rez:
+        print("Ok")
+        tName = os.path.basename(testFolderName)
+        i = 0
+        while True:
+            tFileName = f"{testResultFolder}{tName}-{i}.docx"
+            tLock = testResultFolder + ".~lock." + tName + str(i) + ".docx#"
+            print(tLock, glob.glob(tLock))
+            if glob.glob(tLock):
+                i += 1
+            else:
+                if glob.glob(tFileName):
+                    os.remove(tFileName)
+                break
+        open(tFileName, "wb").write(d2d.docxResultBinary)
+    else:
+        print("Fail")
+print("found Excel format strings:")
 for y in d2d.usedFormatStrings:
-	print (f"{y:20}'{d2d.usedFormatStrings[y]:20}' '{d2d.setNmFmt(d2d.usedFormatStrings[y],y)}'")
-if sys.platform=="win32":
-	os.startfile(testResultFolder.replace("/","\\"))
+    print(f"{y:20}'{d2d.usedFormatStrings[y]:20}' '{d2d.setNmFmt(d2d.usedFormatStrings[y],y)}'")
+if sys.platform == "win32":
+    os.startfile(testResultFolder.replace("/", "\\"))
